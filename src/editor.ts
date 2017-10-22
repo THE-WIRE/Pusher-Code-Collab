9cf4029904722cb87244391ce9e128f3d1f9f43b
Suyog Gadhave <itssuyog96@gmail.com>
2017-10-22 14:34:32 +0530

    added selection fix (temp)

--- src/editor.ts
+++ src/editor.ts
@@ -7,15 +7,17 @@ export class Editor{
      * insert
      */
     public insert(content, range) {
-        let editor = vscode.window.activeTextEditor;
+        let e = vscode.window.activeTextEditor;
+        let _r = new vscode.Range(new vscode.Position(range[0].line, range[0].character), new vscode.Position(range[1].line, range[1].character))
 
-        if(!editor){
+        if(!e){
             vscode.window.showErrorMessage("No open file!!");
             return;
         }
 
-        editor.edit((editBuilder) =>{
-            editBuilder.insert(range, content);
+        e.edit((editBuilder) =>{
+            editBuilder.replace(_r, content);
+            e.selection = new vscode.Selection(new vscode.Position(e.selection.end.line, e.selection.end.character), new vscode.Position(e.selection.end.line, e.selection.end.character + 1))
         });
 
     }
